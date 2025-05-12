c3b62dea81cbb5b69160bf1270c95d78a678cdb1
hy258 <3627923058@qq.com>
2025-05-12 21:36:04 +0800

    完成了键盘和VGA！！！！PA2，堂堂完结！！！

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -4,17 +4,51 @@
 #define SYNC_ADDR (VGACTL_ADDR + 4)
 
 void __am_gpu_init() {
+  uint32_t size_info = inl(VGACTL_ADDR);
+  int w = size_info >> 16;
+  int h = size_info & 0xFFFF;
+  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+
+  for (int i = 0; i < w * h; i++) {
+    fb[i] = i;
+  }
+
+  outl(SYNC_ADDR, 1);
 }
 
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
-  *cfg = (AM_GPU_CONFIG_T) {
-    .present = true, .has_accel = false,
-    .width = 0, .height = 0,
-    .vmemsz = 0
-  };
+  uint32_t size_info = inl(VGACTL_ADDR);
+  uint32_t width = size_info >> 16;
+  uint32_t height = size_info & 0xFFFF;
+
+  *cfg = (AM_GPU_CONFIG_T){.present = true,
+                           .has_accel = false,
+                           .width = width,
+                           .height = height,
+                           .vmemsz = width * height * sizeof(uint32_t)};
 }
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
+  int x = ctl->x, y = ctl->y, w = ctl->w, h = ctl->h;
+  uint32_t *pixels = ctl->pixels;
+  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+
+  uint32_t size_info = inl(VGACTL_ADDR);
+  uint32_t screen_w = size_info >> 16;
+  uint32_t screen_h = size_info & 0xFFFF;
+
+  // 确保不超出屏幕范围
+  int copy_w = (x + w <= screen_w) ? w : screen_w - x;
+  int copy_h = (y + h <= screen_h) ? h : screen_h - y;
+
+  // 逐行复制像素数据到帧缓冲
+  for (int j = 0; j < copy_h; j++) {
+    for (int i = 0; i < copy_w; i++) {
+      fb[(y + j) * screen_w + (x + i)] = pixels[j * w + i];
+    }
+  }
+
+  // 如果需要同步，向同步寄存器写入1
   if (ctl->sync) {
     outl(SYNC_ADDR, 1);
   }
--- nemu/src/device/vga.c
+++ nemu/src/device/vga.c
@@ -72,8 +72,13 @@ static inline void update_screen() {
 #endif
 
 void vga_update_screen() {
-  // TODO: call `update_screen()` when the sync register is non-zero,
-  // then zero out the sync register
+  // 检查同步寄存器是否非零
+  if (vgactl_port_base[1] != 0) {
+    // 调用update_screen()更新屏幕
+    update_screen();
+    // 将同步寄存器归零
+    vgactl_port_base[1] = 0;
+  }
 }
 
 void init_vga() {
